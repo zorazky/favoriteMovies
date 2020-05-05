c99a23157a1b900497d7e59cd37ce06e3a179d25
zorazky <oswaldo.vazquez@gmail.com>
2020-05-05 12:12:15 -0500

    getting user inputs

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,18 +2,57 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
+
+const movies = [];
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
-}
+};
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
     toggleBackdrop();
 };
 
+const clearMovieInput = () => {
+    for (const usrInput of userInputs) {
+        usrInput.value = '';
+    }
+};
+
 const cancelAddMovie = () => {
     toggleMovieModal();
+    clearMovieInput();
+};
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if (
+        titleValue.trim() === '' || 
+        imageUrlValue.trim() === '' || 
+        ratingValue.trim() === '' || 
+        +ratingValue < 1 ||
+        +ratingValue > 5
+    ) {
+        alert('Please enter valid values (rating between 1 and 5).');
+        return;
+    }
+
+    const newMovie = {
+        title: titleValue,
+        image: imageUrlValue,
+        rating: ratingValue
+    };
+
+    movies.push(newMovie);
+    console.log(movies);
+    toggleMovieModal();
+    clearMovieInput();
 }
 
 const backdropClickHandler = () => {
@@ -23,4 +62,5 @@ const backdropClickHandler = () => {
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie );
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
